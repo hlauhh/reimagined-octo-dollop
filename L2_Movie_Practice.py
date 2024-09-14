96052d912bfaa609465bd84dd7487e448e4afd0b
hlauhh <77758458+hlauhh@users.noreply.github.com>
2024-09-14 12:42:20 +0800

    L2 3

--- L2_Movie_Practice.py
+++ L2_Movie_Practice.py
@@ -5,10 +5,11 @@
 # retrieve data
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # pd.set_option('display.max_rows', None)  # Show all rows
 pd.set_option('display.max_columns', None)  # Show all columns
-df_movie = pd.read_csv(r"C:\Users\30A\Downloads\movie_sample_dataset.csv")
+df_movie = pd.read_csv(r'C:\Users\30A\Downloads\movie_sample_dataset.csv')
 df = pd.read_csv('movie_sample_dataset.csv')
 print(df.describe())
 # print(df.head(10), df.tail(10), df.sample(10))
@@ -36,11 +37,50 @@ print(df.describe())
 # 1. find the average gross per country
 # print(df.head())
 # df['gross'] = np.where(condition, action when true, action when false)
-# df['gross'] = np.where(df['gross'].isnull() == True, df['gross'].mean(), df['gross'])
-df['gross'] = np.where(df['gross'].isnull() == True, df[df.country == 'USA']['gross'].mean(), df['gross'])
+df['gross'] = np.where(df['gross'].isnull() == True, df['gross'].mean(), df['gross'])
+# df['gross'] = np.where(df['gross'].isnull() == True, df[df.country == 'USA']['gross'].mean(), df['gross'])
 print(df[df['gross'].isnull() == True])
 
-# remove rows from analysis with missing budget
+# 2. remove rows from analysis with missing budget
 # stick | means Or, & means And
-df2 = df[(df['budget'] > 100)]
-print(df2.shape)
+df2 = df[(df['budget'] > 100)].reset_index(drop=True)
+# print(df2.shape)
+print(df2)
+
+# 3. standardise country names
+print(df['country'].unique())
+df['country'] = np.where(df['country'].isin(['USA', 'usa']), 'United States', df['country'])
+df['country'] = np.where(df['country'] == 'UK', 'United Kingdom', df['country'])
+print(df['country'].unique())
+
+# df['ROI'] = gross / budget
+df['ROI'] = df['gross'] / df['budget']
+
+
+print(df['ROI'].describe())
+# df['ROI'].plot(kind='hist')
+# plt.show()
+
+# df3 = df[df['ROI'] < 1000 ].reset_index(drop=True)
+# df3['ROI'].plot(kind='hist')
+
+df4 = df[df['ROI'] < 20 ].reset_index(drop=True)
+df4['ROI'].plot(kind='hist')
+plt.show()
+
+print(df4.groupby('country')['ROI'].describe())
+
+# splitting column
+print('Leonardo DiCaprio,Matthew McConaughey,Jon Favreau'.split(','))
+# df4['actors'].str.split(',')
+# print(df4)
+
+df_actor = df['actors'].str.split(',', expand=True)
+print(df_actor.columns)
+print(df.columns)
+
+df['Actor_1'] = df_actor[0]
+df['Actor_2'] = df_actor[1]
+df['Actor_3'] = df_actor[2]
+
+print(df.head())
